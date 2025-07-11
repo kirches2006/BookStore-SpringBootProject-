9688c11e87b9cc9506aa537e9cae703384ef28f6
Kirill <kirches2020@mail.ru>
2025-07-11 10:36:26 +0300

    initial commit 2

--- src/main/java/com/greatbit/Application.java
+++ src/main/java/com/greatbit/Application.java
@@ -5,16 +5,18 @@ import com.greatbit.models.BooksStorage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.UUID;
+
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         BooksStorage.getBooks().add(
-                new Book("Три товарища", "Эрих Мария Ремарк", 560)
+                new Book(UUID.randomUUID().toString(), "Три товарища", "Эрих Мария Ремарк", 560)
         );
 
         BooksStorage.getBooks().add(
-                new Book("Богатый Папа, Бедный Папа", "Роберт Киосаки", 300)
+                new Book(UUID.randomUUID().toString(), "Богатый Папа, Бедный Папа", "Роберт Киосаки", 300)
         );
         SpringApplication.run(Application.class, args);
     }
